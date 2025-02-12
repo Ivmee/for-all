e17e677525a2d49a03a23a0842426841e4250eb7
immit <mitin_im@edu.spbau.ru>
2025-02-12 10:43:51 +0300

    trash

---
+++ 1/law.py
@@ -0,0 +1,11 @@
+k=False
+print("определитеь ваше возраста")
+while k!=True:
+    a = int(input(">>>"))
+    if input(">>>") asn:
+
+        a=int(input(">>>"))
+    else:
+        if a>10
+b=int(input(">>>"))
+
---
+++ scratch_1.py
@@ -0,0 +1,12 @@
+import matplotlib
+import pandas
+f=open("company_sallers_data.csv"
+       "")
+
+fig = plt.figure()
+x = np.arange(10)
+y = 2.5 * np.sin(x / 20 * np.pi)
+
+plt.plot(x, y + 3, label='Какой-то синус')
+plt.xlabel("m, кг")
+plt.ylabel("I, А")
