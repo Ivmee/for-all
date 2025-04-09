f0ef419ffe73e60b2b9d0d7f2add2bd9f66e294d
immit <mitin_im@edu.spbau.ru>
2025-04-09 11:10:33 +0300

    добавлен экспорт

--- 1/manim/first-work.py
+++ 1/manim/first-work.py
@@ -7,8 +7,6 @@ class Shapes(Scene):
 
         circle = Circle()
         square = Square()
-        triangle=Polygon(np.array([0,0,0]),np.array([1,1,0]),np.array([1,-1,0]))
-        self.play(Create(circle))
-        self.play(FadeOut(circle))
-        self.play(GrowFromCenter(square))
-        self.play(Transform(square,triangle))
+        self.play(GrowFromCenter(circle))
+        self.play(Transform(circle,square))
+        self.play(FadeOut(square))
